2295b1c6e57751b2c172a723cfb3fe8db4013b8f
Keaton Foster <fosterkeaton@yahoo.com>
2023-01-01 16:52:26 -0700

    Updated lab endpoints

--- api/functions/src/labHandler.ts
+++ api/functions/src/labHandler.ts
@@ -1,8 +1,14 @@
 import { firestore } from 'firebase-admin';
 import { antibodiesCollection, labsCollection } from '../config/database';
+import Crypto from './auth';
 import { db } from './index';
 
 export class LabHandler {
+    private _exists: boolean = false;
+    public get exists(): boolean {
+        return this._exists;
+    }
+
     private _lab: string = '';
     public get lab() {
         return this._lab;
@@ -18,27 +24,104 @@ export class LabHandler {
         if (this._antibodyCollectionRef) return this._antibodyCollectionRef;
         else throw new Error('Antibody Reference not set');
     }
-
-    public constructor(lab: string) {
-        this._lab = lab;
-        this._labRef = this.getLabReference(lab);
-        this._antibodyCollectionRef = this.getAntibodiesReference(lab);
+    public constructor(labName: string);
+    public constructor(labName: string, labRef: firestore.DocumentReference);
+    public constructor(labName: string, labRef?: firestore.DocumentReference) {
+        this._lab = labName;
+        if (labRef) {
+            this._labRef = labRef ?? null;
+        }
     }
-
-    private getLabReference(lab: string): firestore.DocumentReference {
-        return db.collection(labsCollection).doc(lab);
+    public async setReferences() {
+        await this.setLabReference();
+        this.setAntibodiesReference();
     }
-
-    private getAntibodiesReference(lab: string): firestore.CollectionReference {
-        return this.getLabReference(lab).collection(antibodiesCollection);
+    public static async addLab(
+        labName: string,
+        password: string
+    ): Promise<LabHandler> {
+        const check = await LabHandler.checkExists(labName);
+        if (check.exists) {
+            throw new Error('Lab Exists');
+        }
+        const labRef = await db.collection(labsCollection).add({
+            name: labName,
+            password: Crypto.createHash(password)
+        });
+        return new LabHandler(labName, labRef);
     }
-
-    //TODO: REMOVE THIS AND MODIFY TO HASHING
-    async checkPassword(password: string) {
-        return true; //always pass for now... during testing
-        // const labData: firestore.DocumentData = await this.labRef.get();
-        // const validPassword: string = labData.get('password');
-        // return validPassword === password;
+    public static async checkExists(labName: string): Promise<{
+        exists: boolean;
+        snapshot: firestore.QuerySnapshot<firestore.DocumentData>;
+    }> {
+        const snapshot = await LabHandler.getLabSnapshot(labName);
+        return {
+            exists: snapshot.docs.length === 1,
+            snapshot: snapshot
+        };
+    }
+    public static async getLabSnapshot(
+        labName: string
+    ): Promise<firestore.QuerySnapshot<firestore.DocumentData>> {
+        const query = db
+            .collection(labsCollection)
+            .where('name', '==', labName);
+        return await query.get();
+    }
+    public async changeLabName(newLabName: String) {
+        if (this.exists) {
+            this.labRef.update({
+                name: newLabName
+            });
+        } else {
+            throw new Error('Lab does not exist');
+        }
+    }
+    public async changePassword(newPassword: string) {
+        if (this.exists) {
+            this.labRef.update({
+                password: Crypto.createHash(newPassword)
+            });
+        } else {
+            throw new Error('Lab does not exist');
+        }
+    }
+    private async setLabReference() {
+        if (this.labRef) {
+            const validLabRef = await this.verifyLabReference();
+            this._exists = validLabRef;
+            if (validLabRef) return;
+        }
+        const check = await LabHandler.checkExists(this.lab);
+        this._exists = check.exists;
+        if (check.exists) {
+            this._labRef = check.snapshot.docs[0].ref;
+        }
+    }
+    private async verifyLabReference(): Promise<boolean> {
+        const labSnapshot = await this.labRef.get();
+        const exists = labSnapshot.exists;
+        let labName = '';
+        if (exists) {
+            labName = labSnapshot.get('name');
+        }
+        return exists && labName === this.lab;
+    }
+    private setAntibodiesReference() {
+        if (this.exists) {
+            this._antibodyCollectionRef =
+                this.labRef.collection(antibodiesCollection);
+        } else {
+            this._antibodyCollectionRef = null;
+        }
+    }
+    public async checkPassword(password: string) {
+        if (this.exists) {
+            const labData: firestore.DocumentData = await this.labRef.get();
+            const validPassword: string = labData.get('password');
+            return validPassword === Crypto.createHash(password);
+        }
+        return false;
     }
 
     // authenticate(password: string) {}
--- api/functions/src/routes/lab.ts
+++ api/functions/src/routes/lab.ts
@@ -1,20 +1,48 @@
 import * as express from 'express';
+import { Authenticate } from '../auth';
+import { LabHandler } from '../labHandler';
 
 const LabRouter = express.Router();
 
 //TODO: add lab
-LabRouter.post('/', (req, resp) => {
-    resp.status(200).send('Test, no action taken - POST lab');
+LabRouter.post('/', async (req, res) => {
+    try {
+        const labHandler = await LabHandler.addLab(
+            req.body.lab,
+            req.body.password
+        );
+        res.status(200).send({
+            msg: 'Lab added',
+            id: labHandler.labRef.id
+        });
+    } catch (error) {
+        res.status(500).send(error);
+    }
 });
 
-//TODO: delete lab
-LabRouter.delete('/', (req, resp) => {
-    resp.status(200).send('Test, no action taken - POST lab');
+LabRouter.use(Authenticate);
+LabRouter.delete('/', async (req, res) => {
+    try {
+        const labHandler = res.locals.labHandler as LabHandler;
+        labHandler.labRef.delete();
+        labHandler.setReferences(); //reset references since it no longer exists.
+        res.status(200).send({
+            msg: 'Lab successfully deleted'
+        });
+    } catch (error) {
+        res.status(500).send(error);
+    }
 });
-
-//TODO: edit labname
-LabRouter.put('/', (req, resp) => {
-    resp.status(200).send('Test, no action taken - PUT lab');
+LabRouter.put('/', async (req, res) => {
+    try {
+        const labHandler = res.locals.labHandler as LabHandler;
+        await labHandler.labRef.update(req.body.new);
+        res.status(200).send({
+            msg: 'Fields changed successfully'
+        });
+    } catch (error) {
+        res.status(500).send(error);
+    }
 });
 
 export default LabRouter;
