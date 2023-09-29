7368779db08c19a51f36c95e623f3cdb4737bea4
Keaton Foster <fosterkeaton@yahoo.com>
2023-09-29 10:33:54 -0600

    Add prd baseURL set

--- gui/src/App.tsx
+++ gui/src/App.tsx
@@ -9,9 +9,7 @@ import { CssBaseline } from '@mui/material';
 import { Route, HashRouter, Routes, Navigate } from 'react-router-dom';
 import { LoginForm } from './Components/Login/Login';
 import axios from 'axios';
-
-export const baseURL = //'https://us-central1-antibody-inventory-assistant.cloudfunctions.net/api';
-    'http://127.0.0.1:5001/antibody-inventory-assistant/us-central1/api';
+import { baseURL } from './baseurl';
 
 axios.defaults.baseURL = baseURL;
 axios.defaults.maxRedirects = 0;
